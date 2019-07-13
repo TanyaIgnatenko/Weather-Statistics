2909283e3a5d583390f9245813710d65b97f4bb2
TanyaIgnatenko <alisainy@gmail.com>
2019-07-13 16:44:41 +0700

    Parse data

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ import { Point } from './point.js';
 
 'use strict';
 
-const points = [
-  new Point(75, 50),
-  new Point(100, 75),
-  new Point(125, 25),
-  new Point(150, 50),
-  new Point(200, 75),
-  new Point(475, 50),
-  new Point(500, 75),
-  new Point(525, 25),
-  new Point(550, 50),
-  new Point(600, 75),
-];
+(async function () {
+  const temperatureData = await fetch('./data/temperature.json')
+    .then(data => data.json());
 
-const canvas = document.getElementById('chart');
+  const chartData = temperatureData.map(data => {
+    const date = new Date(data.t).getTime();
+    const temperature = data.v;
+    return new Point(date, temperature);
+  });
 
-const chart = new Chart();
-chart.drawChart(canvas, points);
+  const canvas = document.getElementById('chart');
+
+  const chart = new Chart();
+  chart.drawChart(canvas, chartData);
+})();
