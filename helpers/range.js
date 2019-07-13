453c4900c9655840923706b6e46fddfb3168348a
TanyaIgnatenko <alisainy@gmail.com>
2019-07-13 22:19:07 +0700

    Make range func return range that includes end

--- helpers/range.js
+++ helpers/range.js
@@ -1,5 +1,5 @@
 function range(start, end, step = 1) {
-  const size = (end - start) / step;
+  const size = (end - start + 1) / step;
   return Array.from({length: size}, (val, i) => i * step + start);
 }
 
